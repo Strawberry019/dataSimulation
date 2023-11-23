fa92f69eaa322714491c9c336ac822481069ca75
Strawberry019 <953757425@qq.com>
2023-11-23 15:16:24 +0800

    try fo fix the bug of deserialization.not finish.

--- src/main/java/com/constraintPreprocess/Serial.java
+++ src/main/java/com/constraintPreprocess/Serial.java
@@ -1,6 +1,8 @@
 package com.constraintPreprocess;
 
+import com.dataSimulation.model.CompAffinityGroup;
 import com.dataSimulation.model.UserInfo;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
@@ -17,12 +19,21 @@ public class Serial {
         // 解析userInfo.json
         JsonNode userInfoJsonNode = objectMapper.readTree(userInfoFile);
 
-        // 解析cloudInfo.json
-        JsonNode cloudInfoJsonNode = objectMapper.readTree(cloudInfoFile);
+        // 使用 TypeReference 来指定数组类型
+        TypeReference<CompAffinityGroup[]> compAffinityGroupType = new TypeReference<CompAffinityGroup[]>() {};
 
-        //测试是否能成功的反序列化得到java对象
-        //UserInfo u = objectMapper.treeToValue(userInfoJsonNode, UserInfo.class);
-        UserInfo u = objectMapper.readValue(userInfoFile, UserInfo.class);
+        // 从 userInfoJsonNode 中获取 comp_affinity_groups 数组
+        CompAffinityGroup[] compAffinityGroups = objectMapper.convertValue(userInfoJsonNode.get("comp_affinity_groups"), compAffinityGroupType);
+
+        // 创建 UserInfo 对象并设置 comp_affinity_groups 属性
+        UserInfo u = new UserInfo();
+
+        // 遍历数组，并逐个添加到 UserInfo 对象中
+        for (CompAffinityGroup compAffinityGroup : compAffinityGroups) {
+            u.setComp_affinity_groups(compAffinityGroup);;
+        }
+
+        // 设置其他属性...
         System.out.println(u.getGreen_level());
     }
 }
